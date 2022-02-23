c1c4899e3ffabbc5b0b7e11a0b874383e8936f97
Darko Vukovic <vdarkobar@gmail.com>
2022-02-23 21:54:50 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+clear
+echo -ne "${RED}Enter Time Zone: "; read TZONE; \
+echo -ne "${RED}Enter PUID: "; read PUID; \
+echo -ne "${RED}Enter PGID: "; read PGID; \
+echo -ne "${RED}Enter Calibre-web Port Number (8083): "; read CPORT; \
+sed -i "s|01|${TZONE}|" .env && \
+sed -i "s|02|${PUID}|" .env && \
+sed -i "s|03|${PGID}|" .env && \
+sed -i "s|04|${CPORT}|" .env && \
+rm README.md && \
+while true; do
+    read -p "Execute 'docker-compose up -d' now? (y/n)" yn
+    case $yn in
+        [Yy]* ) sudo docker-compose up -d; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
